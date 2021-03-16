e5e6227252c2768a32119c6244375268e5a55dec
Jack Setrak <jackfire78@yahoo.com>
2021-03-16 07:53:05 -0700

    Heroku Loggin
    Database connection fix

--- Web Notes/app/Services/BusinessServices/NotesService.php
+++ Web Notes/app/Services/BusinessServices/NotesService.php
@@ -50,13 +50,13 @@ class NotesService {
 		// Azure database
 		// $db = new mysqli("localhost", "azure", "6#vWHD_$", "webnotes", "53217");
 		// Heroku database
-		// $db = new mysqli ( "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "fahczpdegdfdk58l", "da4ukk3v4t1lddor", "ozgf8unmfu7uuiad" );
+		$db = new mysqli ( "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "fahczpdegdfdk58l", "da4ukk3v4t1lddor", "ozgf8unmfu7uuiad" );
 		// AWS database
 		// $db = new mysqli ( "webnotes-jack.cocq0qavoe0i.us-east-2.rds.amazonaws.com", "admin", "adminpassword", "awswebnotes" );
 		// Google Cloud database
 		// $db = new mysqli ( "root", "root", "webnotes_jack", "/cloudsql/smooth-ripple-304314:us-central1:webnotes-jack" );
 		// local testing database
-		$db = new mysqli("localhost", "root", "root", "webnotes");
+		// $db = new mysqli("localhost", "root", "root", "webnotes");
 
 		// Check connection
 		if ($db->connect_errno) {
